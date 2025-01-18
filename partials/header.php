9f8f839a7c21e174a85488b7c2350305cd3f034c
jonasradke <me@jonasradke.com>
2025-01-18 17:46:47 +0100

    add favicon link to header

--- partials/header.php
+++ partials/header.php
@@ -1,4 +1,9 @@
 <!-- partials/header.php -->
+
+<head>
+    <link rel="icon" href="assets/favicon.ico" type="image/x-icon">
+</head>
+
 <nav class="navbar navbar-expand-lg">
     <div class="container d-flex justify-content-center">
         <a class="navbar-brand" href="../">
